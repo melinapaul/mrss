64447a72ff7ddd7ad1a6658667e54fc69b986d71
melinapaul <melina.14691@gmail.com>
2015-04-27 21:51:25 -0700

    Update profile.blade.php

--- resources/views/doctor/profile.blade.php
+++ resources/views/doctor/profile.blade.php
@@ -22,12 +22,13 @@
             @if(!is_null($url))
             <img src="{{$url}}">
             @else
-            <i class="user icon"></i>
+            <i class="user icon" style="font-size:128px;"></i>
             @endif
           </div>
           <div class="content">
             <a class="header" style="font-size: 24px;">{{$doctor->get('name')}}</a><br>
             <div class="meta" style="font-size: 16px;">
+              <div>Doctor ID: <b>{{$doctor->getObjectId()}}</b></div><br>
               <div><b><i class="fa fa-at"></i> Email:</b> {{$doctor->get('email')}}</div><br>
               <div><b><i class="fa fa-calendar"></i> Date of Birth:</b> {{$doctor->get('dob')}}</div><br>
               <div><b><i class="fa fa-user"></i> Gender:</b> {{$doctor->get('gender')}}</div><br>
